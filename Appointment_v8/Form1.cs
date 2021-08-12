44a416fa1fb14d755ee41c01107ee06fd99ada4e
GO SAY BENG <gosaybeng@gmail.com>
2021-08-13 01:28:00 +0800

    Add project files.

---
+++ Appointment_v8/Form1.cs
@@ -0,0 +1,43 @@
+﻿using DevExpress.XtraScheduler;
+using System;
+
+namespace Appointment_v8
+{
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'managementDataSet.Appointments' table. You can move, or remove it, as needed.
+            this.appointmentsTableAdapter.Fill(this.managementDataSet.Appointments);
+
+            schedulerControl1.Storage.Appointments.CommitIdToDataSource = false;
+
+        }
+        #region #Appointment
+        private void schedulerDataStorage1_AppointmentsChanged(object sender, DevExpress.XtraScheduler.PersistentObjectsEventArgs e)
+        {
+            CommitTask();
+        }
+        private void schedulerDataStorage1_AppointmentsDeleted(object sender, DevExpress.XtraScheduler.PersistentObjectsEventArgs e)
+        {
+            CommitTask();
+        }
+        private void schedulerDataStorage1_AppointmentsInserted(object sender, DevExpress.XtraScheduler.PersistentObjectsEventArgs e)
+        {
+            CommitTask();
+        }
+        void CommitTask()
+        {
+            appointmentsTableAdapter.Update(managementDataSet);
+            managementDataSet.AcceptChanges();
+        }
+        #endregion #Appointment
+
+    }
+}
